7145e9858e4658cbea7a92eadf3b282826bd2b1e
Toni Alatalo <toni@playsign.net>
2014-04-14 13:26:28 +0300

    menu with jquery (ui?)

--- map.js
+++ map.js
@@ -105,3 +105,19 @@ function addHandler(map, marker, infowindow, text) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+/*  jQuery ready function. Specify a function to execute when the DOM is fully loaded.  */
+$(document).ready(
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+    /* Next part of code handles hovering effect and submenu appearing */
+    $('.nav li').hover(
+      function () { //appearing on hover
+        $('ul', this).fadeIn();
+      },
+      function () { //disappearing on hover
+        $('ul', this).fadeOut();
+      }
+    );
+  }
+);
